96e68fba6b02ed2ce2606ef98115999feea42e3e
LihaoDing <71325581+LihaoDing@users.noreply.github.com>
2024-12-04 19:12:25 +0800

    Update Image.h

--- src/Image.h
+++ src/Image.h
@@ -1,14 +1,3 @@
-/*
- * Group Name: Ukkonen
- * Members:
- * - Zeyu Zhao (@edsml-zz2123)
- * - Ark Saini (@acse-as12123)
- * - Lihao Ding (@acse-ld823)
- * - Geyu JI (@acse-gj23)
- * - Yanan Wang (@acse-yy3123)
- * - Chandrasekhar Gudipati (@edsml-cg1123)
- */
-
 #include <string>
 #include "Filter.h"
 
